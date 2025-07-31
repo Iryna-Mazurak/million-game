de5998c8cdf1a23be928ef350acd25e6e320f8ee
mazurakiryna <mazurak.iryna@gmail.com>
2025-08-01 00:18:37 +0300

    Preload for buttons backgrounds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import type { Metadata } from 'next';
 import { GameProvider } from '@/features/game/GameProvider';
 import '@/styles/globals.css';
@@ -22,6 +23,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="../images/button-hover.png"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="../images/button-selected.png"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="../images/button-correct.png"
+          type="image/png"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="../images/button-wrong.png"
+          type="image/png"
+        />
+      </Head>
       <body>
         <GameProvider>{children}</GameProvider>
       </body>
