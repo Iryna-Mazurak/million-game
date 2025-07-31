e04ac18c15e32eb528eb12353ec5d0a8a1649e4d
mazurakiryna <mazurak.iryna@gmail.com>
2025-08-01 00:21:30 +0300

    Remove Preload for buttons backgrounds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import type { Metadata } from 'next';
 import { GameProvider } from '@/features/game/GameProvider';
 import '@/styles/globals.css';
@@ -23,32 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
-      <Head>
-        <link
-          rel="preload"
-          as="image"
-          href="../images/button-hover.png"
-          type="image/png"
-        />
-        <link
-          rel="preload"
-          as="image"
-          href="../images/button-selected.png"
-          type="image/png"
-        />
-        <link
-          rel="preload"
-          as="image"
-          href="../images/button-correct.png"
-          type="image/png"
-        />
-        <link
-          rel="preload"
-          as="image"
-          href="../images/button-wrong.png"
-          type="image/png"
-        />
-      </Head>
       <body>
         <GameProvider>{children}</GameProvider>
       </body>
